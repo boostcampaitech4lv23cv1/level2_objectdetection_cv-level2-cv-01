bd5acf28646debb9973ab463f154b0eb927dbf68
jun <quasar0529@gmail.com>
2022-11-22 08:33:29 +0000

    fix: _base_/dataset,schedules

---
+++ mmdetection/configs/faster_rcnn_swin.py
@@ -0,0 +1,6 @@
+_base_ = [
+    "./_base_/dataset/coco_detection.py",
+    "./_base_/models/faster_rcnn_r50_fpn.py",
+    "./_base_/schedulers/schedule_1x.py",
+    "./_base_/default_runtime.py",
+]
