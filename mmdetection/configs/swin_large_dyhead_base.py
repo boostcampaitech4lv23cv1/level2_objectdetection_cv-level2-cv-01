40e9757e62b14e9ab59bca747b6356ac9831b889
jun <quasar0529@gmail.com>
2022-11-29 05:52:22 +0000

    exp-swin aug and cv

---
+++ mmdetection/configs/swin_large_dyhead_base.py
@@ -0,0 +1,304 @@
+# _base_ = '../_base_/default_runtime.py'
+_base_ = "/opt/ml/level2_objectdetection_cv-level2-cv-01/mmdetection/configs/_base_/default_runtime.py"
+
+pretrained = "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth"  # noqa
+model = dict(
+    type="ATSS",
+    backbone=dict(
+        type="SwinTransformer",
+        pretrain_img_size=384,
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(1, 2, 3),
+        # Please only add indices that would be used
+        # in FPN, otherwise some parameter will not be used
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type="Pretrained", checkpoint=pretrained),
+    ),
+    neck=[
+        dict(
+            type="FPN",
+            in_channels=[384, 768, 1536],
+            out_channels=256,
+            start_level=0,
+            add_extra_convs="on_output",
+            num_outs=5,
+        ),
+        dict(
+            type="DyHead",
+            in_channels=256,
+            out_channels=256,
+            num_blocks=6,
+            # disable zero_init_offset to follow official implementation
+            zero_init_offset=False,
+        ),
+    ],
+    bbox_head=dict(
+        type="ATSSHead",
+        num_classes=10,
+        in_channels=256,
+        pred_kernel_size=1,  # follow DyHead official implementation
+        stacked_convs=0,
+        feat_channels=256,
+        anchor_generator=dict(
+            type="AnchorGenerator",
+            ratios=[1.0],
+            octave_base_scale=8,
+            scales_per_octave=1,
+            strides=[8, 16, 32, 64, 128],
+            center_offset=0.5,
+        ),  # follow DyHead official implementation
+        bbox_coder=dict(
+            type="DeltaXYWHBBoxCoder",
+            target_means=[0.0, 0.0, 0.0, 0.0],
+            target_stds=[0.1, 0.1, 0.2, 0.2],
+        ),
+        loss_cls=dict(
+            type="FocalLoss", use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=1.0
+        ),
+        loss_bbox=dict(type="GIoULoss", loss_weight=2.0),
+        loss_centerness=dict(
+            type="CrossEntropyLoss", use_sigmoid=True, loss_weight=1.0
+        ),
+    ),
+    # training and testing settings
+    train_cfg=dict(
+        assigner=dict(type="ATSSAssigner", topk=9),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False,
+    ),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type="nms", iou_threshold=0.6),
+        max_per_img=100,
+    ),
+)
+
+# dataset settings
+dataset_type = "CocoDataset"
+data_root = "/opt/ml/dataset/"
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True
+)
+albu_train_transforms = [
+    dict(
+        type="ShiftScaleRotate",
+        shift_limit=0.0625,
+        scale_limit=0.0,
+        rotate_limit=0,
+        interpolation=1,
+        p=0.5,
+    ),
+    dict(
+        type="RandomBrightnessContrast",
+        brightness_limit=[0.1, 0.3],
+        contrast_limit=[0.1, 0.3],
+        p=0.2,
+    ),
+    dict(
+        type="OneOf",
+        transforms=[
+            dict(
+                type="RGBShift",
+                r_shift_limit=10,
+                g_shift_limit=10,
+                b_shift_limit=10,
+                p=1.0,
+            ),
+            dict(
+                type="HueSaturationValue",
+                hue_shift_limit=20,
+                sat_shift_limit=30,
+                val_shift_limit=20,
+                p=1.0,
+            ),
+        ],
+        p=0.1,
+    ),
+    dict(type="GaussNoise", p=0.5, var_limit=(0.0, 26.849998474121094)),
+    dict(type="JpegCompression", quality_lower=85, quality_upper=95, p=0.2),
+    dict(type="ChannelShuffle", p=0.1),
+    dict(
+        type="OneOf",
+        transforms=[
+            dict(type="Blur", blur_limit=3, p=1.0),
+            dict(type="MedianBlur", blur_limit=3, p=1.0),
+        ],
+        p=0.1,
+    ),
+]
+
+train_pipeline_default = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations", with_bbox=True),
+    dict(
+        type="Resize",
+        img_scale=[(768, 1024), (1024, 768)],
+        multiscale_mode="range",
+        keep_ratio=True,
+        backend="pillow",
+    ),
+    dict(type="RandomFlip", flip_ratio=0.5),
+    dict(type="Normalize", **img_norm_cfg),
+    dict(type="Pad", size_divisor=128),
+    dict(type="DefaultFormatBundle"),
+    dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
+]
+train_pipeline_1 = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations", with_bbox=True),
+    dict(type="RandomFlip", flip_ratio=0.5),
+    dict(type="MinIoURandomCrop"),
+    dict(
+        type="Resize",
+        img_scale=[(768, 1024), (1024, 768)],
+        multiscale_mode="range",
+        keep_ratio=True,
+        backend="pillow",
+    ),
+    dict(
+        type="PhotoMetricDistortion"
+    ),  # 밝기, 대조, HSV shift, saturation, hue, RGB shift, 등등 p=0.5로 랜덤으로 적용
+    dict(type="Corrupt", corruption="motion_blur"),  # noise, blur 를 corruption으로 지정
+    dict(type="Normalize", **img_norm_cfg),
+    dict(type="Pad", size_divisor=128),
+    dict(type="DefaultFormatBundle"),
+    dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
+]
+train_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations", with_bbox=True),
+    dict(
+        type="Resize",
+        img_scale=[(768, 1024), (1024, 768)],
+        multiscale_mode="range",
+        keep_ratio=True,
+        backend="pillow",
+    ),
+    dict(type="Pad", size_divisor=128),
+    dict(
+        type="Albu",
+        transforms=albu_train_transforms,
+        bbox_params=dict(
+            type="BboxParams",
+            format="pascal_voc",
+            label_fields=["gt_labels"],
+            min_visibility=0.0,
+            filter_lost_elements=True,
+        ),
+        keymap={"img": "image", "gt_bboxes": "bboxes"},
+        update_pad_shape=False,
+        skip_img_without_anno=True,
+    ),
+    dict(type="RandomFlip", flip_ratio=0.5),
+    dict(type="Normalize", **img_norm_cfg),
+    dict(type="DefaultFormatBundle"),
+    dict(type="Collect", keys=["img", "gt_bboxes", "gt_labels"]),
+]
+test_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(
+        type="MultiScaleFlipAug",
+        img_scale=(1024, 768),
+        flip=True,
+        transforms=[
+            dict(type="Resize", keep_ratio=True, backend="pillow"),
+            dict(type="RandomFlip"),
+            dict(type="Normalize", **img_norm_cfg),
+            dict(type="Pad", size_divisor=128),
+            dict(type="ImageToTensor", keys=["img"]),
+            dict(type="Collect", keys=["img"]),
+        ],
+    ),
+]
+
+classes = (
+    "General trash",
+    "Paper",
+    "Paper pack",
+    "Metal",
+    "Glass",
+    "Plastic",
+    "Styrofoam",
+    "Plastic bag",
+    "Battery",
+    "Clothing",
+)
+
+# Use RepeatDataset to speed up training
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type="RepeatDataset",
+        times=2,
+        dataset=dict(
+            type=dataset_type,
+            # ann_file=data_root + 'CV_strategy/train_f1.json',
+            ann_file="/opt/ml/level2_objectdetection_cv-level2-cv-01/eda/train_newf4.json",
+            img_prefix=data_root,
+            classes=classes,
+            pipeline=train_pipeline,
+        ),
+    ),
+    val=dict(
+        type=dataset_type,
+        # ann_file=data_root + 'CV_strategy/valid_f0.json',
+        ann_file="/opt/ml/level2_objectdetection_cv-level2-cv-01/eda/valid_newf4.json",
+        img_prefix=data_root,
+        classes=classes,
+        pipeline=test_pipeline,
+    ),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + "test.json",
+        # ann_file=data_root + 'cv_by_bbox/valid_bbox0.json',
+        img_prefix=data_root,
+        classes=classes,
+        pipeline=test_pipeline,
+    ),
+)
+evaluation = dict(interval=1, metric="bbox", save_best="bbox_mAP_50")
+
+# optimizer
+optimizer_config = dict(grad_clip=None)
+optimizer = dict(
+    type="AdamW",
+    lr=0.00005,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys={
+            "absolute_pos_embed": dict(decay_mult=0.0),
+            "relative_position_bias_table": dict(decay_mult=0.0),
+            "norm": dict(decay_mult=0.0),
+        }
+    ),
+)
+
+# learning policy
+lr_config = dict(
+    policy="step", warmup="linear", warmup_iters=500, warmup_ratio=0.001, step=[8, 11]
+)
+runner = dict(type="EpochBasedRunner", max_epochs=24)
+
+# lr_config = dict(
+#     policy="CosineAnnealing",
+#     warmup="linear",
+#     warmup_iters=1000,
+#     warmup_ratio=0.01,
+#     min_lr=4e-6,
+# )
